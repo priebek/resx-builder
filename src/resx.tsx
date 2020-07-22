803fceedd38a4745f41dca4a461ec75f4c61d75f
Bjarne <bjarne_priebe@hotmail.com>
2020-07-22 20:45:36 +0200

    merge resx build

--- src/resx.tsx
+++ src/resx.tsx
@@ -1,30 +1,34 @@
 import { XML, XMLList } from "sxml";
 
+const generateResx = (name: string, translate: string): string => {
+  let build = '<?xml version="1.0" encoding="utf-8"?>\n';
+  build += "<root>\n";
+  build += `<data name="${name}" xml:space="preserve">\n`;
+  build += `<value>${translate}</value>\n`;
+  build += "</data>\n";
+  build += "</root>\n";
+  return build;
+};
+
 export default function getResxStructure(): string {
-  const xml = new XML(s);
+  const xml = new XML(template(generateResx("dog", "Perro")));
 
   const xmlList = xml.get("data");
-
   console.log("#" + xmlList.size());
-
-  console.log(
-    xmlList.at(0).getProperty("name")
-  );
+  console.log(xmlList.at(0).getProperty("name"));
 
   const members: XMLList = xml.get("data").at(0).get("value");
 
   members.at(0).setValue("changed!");
 
-  console.log(
-    members.at(0).getValue()
-  );
-
+  console.log(members.at(0).getValue());
   console.log(xml.toString());
 
-  return "";
+  return xml.toString();
 }
 
-const s = `<?xml version="1.0" encoding="utf-8"?>
+const template = (input: string): string => {
+  return `<?xml version="1.0" encoding="utf-8"?>
 <root>
   <xsd:schema id="root" xmlns="" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
     <xsd:import namespace="http://www.w3.org/XML/1998/namespace" />
@@ -87,5 +91,7 @@ const s = `<?xml version="1.0" encoding="utf-8"?>
   <data name="myName" xml:space="preserve">
     <value>myValue</value>
   </data>
+  ${input}
 </root>
-`
+`;
+};
